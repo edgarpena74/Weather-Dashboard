fd18aac175308e1eb3ac955d8de3d7037641e855
Edgar Pena <edgarignaciopena@gmail,com>
2020-11-09 18:36:03 -0800

    citys can be added and api fetches data

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let user = document.getElementById("user")
+let searchCity = document.getElementById("searchCity")
 let button = document.getElementById("searchButton")
-let userCity = localStorage.getItem("City")
+let cityList = document.getElementsByClassName("cityList")
+let list = document.getElementById("list")
 
 
 console.log("before click func")
@@ -8,14 +10,31 @@ console.log("before click func")
 function getCity() {
     console.log("search was clicked")
     let city = user.value;
-    window.localStorage.setItem("City", city )
+    // window.localStorage.setItem("City", city )
+
+    
+
+    let URL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=de8be824ac11480024fa9aeef3ea6f60"
+
+    fetch(URL)
+        .then(response => response.json())
+        .then(data => console.log(data))
 
 
+    let addCity = document.createElement("li");
+    addCity.textContent = city;
+    list.prepend(addCity);
+
+    
 }
 
 console.log("after click")
 
 button.addEventListener("click", getCity)
+searchCity.addEventListener("submit", function(event){
+    preventDefault(event);
+})
+
 
 
 // $("#searchCity").on("click",
